dc7d7c321b8fa1536ce8c05829ebf58255fb9415
clayton <claystamper@gmail.com>
2019-11-26 14:54:27 -0600

    added png of notes for readme

--- CNN.py
+++ CNN.py
@@ -1,1 +1,12 @@
-print ("Hello CNN")
+from keras.models import Sequential
+from keras.layers import Convolution2D  # for 2d images
+from keras.layers import MaxPool2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+cnn = Sequential()
+
+# step 1: convolution
+# slide feature detectors ("filters") along image
+# results feature maps that form convolutional layer
+cnn.add(Convolution2D())
