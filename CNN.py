2ae55d0490169e04d95e050af0cb9350032d88b8
clayton <claystamper@gmail.com>
2019-11-26 15:14:48 -0600

    cnn compiled

--- CNN.py
+++ CNN.py
@@ -9,4 +9,19 @@ cnn = Sequential()
 # step 1: convolution
 # slide feature detectors ("filters") along image
 # results feature maps that form convolutional layer
-cnn.add(Convolution2D())
+cnn.add(Convolution2D(32, 3, 3, input_shape=(64, 64, 3), activation='relu'))  # 32, 3x3 filters
+
+# step 2: pooling
+cnn.add(MaxPool2D(pool_size=(2, 2)))
+
+# step 3: flatten
+# this vector will be the input of a future ann
+cnn.add(Flatten())
+
+# step 4: full connection
+cnn.add(Dense(output_dim=128, activation='relu'))  # add hidden layers
+cnn.add(Dense(output_dim=1, activation='sigmoid'))  # sigmoid for binary output
+
+# compile cnn
+cnn.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
