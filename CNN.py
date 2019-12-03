4d1c9d805116655150ff3f929861382d60173187
clayton <claystamper@gmail.com>
2019-12-03 16:28:13 -0600

    ready to train

--- CNN.py
+++ CNN.py
@@ -3,13 +3,17 @@ from keras.layers import Convolution2D  # for 2d images
 from keras.layers import MaxPool2D
 from keras.layers import Flatten
 from keras.layers import Dense
+import tensorflow as tf
+from keras_preprocessing.image import ImageDataGenerator
 
 cnn = Sequential()
+
+rgb = 64
 
 # step 1: convolution
 # slide feature detectors ("filters") along image
 # results feature maps that form convolutional layer
-cnn.add(Convolution2D(32, 3, 3, input_shape=(64, 64, 3), activation='relu'))  # 32, 3x3 filters
+cnn.add(Convolution2D(32, 3, 3, input_shape=(rgb, rgb, 3), activation='relu'))  # 32, 3x3 filters
 
 # step 2: pooling
 cnn.add(MaxPool2D(pool_size=(2, 2)))
@@ -25,3 +29,34 @@ cnn.add(Dense(output_dim=1, activation='sigmoid'))  # sigmoid for binary output
 # compile cnn
 cnn.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
+# image augmentation - prevent overfitting
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_set = train_datagen.flow_from_directory(
+        'dataset/training_set',
+        target_size=(rgb, rgb),
+        batch_size=32,
+        class_mode='binary')
+
+test_set = test_datagen.flow_from_directory(
+        'dataset/test_set',
+        target_size=(rgb, rgb),
+        batch_size=32,
+        class_mode='binary')
+
+cnn.fit_generator(
+        train_set,
+        steps_per_epoch=8000, # we have 8k images in our training set
+        epochs=10,
+        validation_data=test_set,
+        validation_steps=2000)
+
+print(cnn.summary())
+
+cnn.save('CatDogModel.h5')
